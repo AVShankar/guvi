d3c8c3d8755d83ef40664e51a2c3602c477f85b2
Shankar <52577232+AVShankar@users.noreply.github.com>
2019-07-17 14:15:29 +0530

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,12 +1,17 @@
-#include<stdio.h>
-void main()
+#include <stdio.h>
+#include <stdbool.h>
+int main(void)
 {
-int a;
-scanf("%d",&a);
-if(a%2==0)
-printf("Even");
-else if(a%2!=0)
-printf("Odd");
-else
-printf("invalid");
+    int a;
+    scanf("%d", &a);
+    if(a>0)
+    {
+        if(a%2==0)
+        printf("Even");
+        else 
+        printf("Odd");
+    }
+    else
+    printf("Invalid");
+    return 0;
 }
