93413feb8fb0550c9f7b679213dbdc7b4c7cbb94
Shankar <52577232+AVShankar@users.noreply.github.com>
2019-07-17 14:23:47 +0530

    Update 3.c

--- 3.c
+++ 3.c
@@ -3,10 +3,13 @@ int main()
 {
 char ch;
 scanf("%c", &ch);
-if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U')
-printf("Vowel");
-elseif(ch=!'a' || ch=!'e' || ch=!'i' || ch=!'o' || ch=!'u' || ch=!'A' || ch=!'E' || ch=!'I' || ch=!'O' || ch=!'U')
-printf("Consonant");
+if( (ch>='a' && ch<='z') || (ch>='A' && ch<='Z'))
+{
+  if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U')
+  printf("Vowel");
+  else 
+  printf("Consonant");
+}
 else
 printf("invalid");
 return 0;
